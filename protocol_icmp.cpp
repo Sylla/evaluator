a478c2da5efdab7965688ed0f86848456d9e674f
Amir <root@amandegar.(none)>
2011-10-30 19:12:19 +0330

    add ICMP protocol to capture and statistic.. :) very easy and nice .:D

---
+++ protocol_icmp.cpp
@@ -0,0 +1,19 @@
+#include "protocol_icmp.h"
+
+protocol_icmp::protocol_icmp(statistic_ *stat) : protocolHandler_(stat)
+{
+}
+//-------------------------------------------------------------------
+int
+protocol_icmp::check(u_int16_t protocol, u_int16_t sport, u_int16_t dport)
+{
+    if (protocol == IPPROTO_ICMP)
+        statistic->incrementCounter(CNT_ICMP);
+}
+//-------------------------------------------------------------------
+int
+protocol_icmp::process()
+{
+
+}
+//-------------------------------------------------------------------
---
+++ protocol_icmp.h
@@ -0,0 +1,16 @@
+#ifndef PROTOCOL_ICMP_H
+#define PROTOCOL_ICMP_H
+
+#include "protocolhandler.h"
+#include <netinet/ip.h>
+
+class protocol_icmp : public protocolHandler_
+{
+public:
+    protocol_icmp(statistic_ *stat);
+    int check(u_int16_t protocol, u_int16_t sport, u_int16_t dport);
+    int process();
+
+};
+
+#endif // PROTOCOL_ICMP_H
