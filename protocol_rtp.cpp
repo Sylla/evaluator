b62d012e82c13dd573b8cf6dac52aaaf89622931
Amir <root@amandegar.(none)>
2011-12-17 19:25:16 +0330

    add rfc2250 capture and save data

--- protocol_rtp.cpp
+++ protocol_rtp.cpp
@@ -1,7 +1,14 @@
 #include "protocol_rtp.h"
 
-protocol_rtp::protocol_rtp(u_int16_t sport, u_int16_t dport,statistic_ *stat) : protocolHandler_(sport, dport, stat)
+protocol_rtp::protocol_rtp(u_int16_t sport, u_int16_t dport,statistic_ *stat) : protocolHandler_(sport, dport, stat), rtpFile("RTP2250.log")
 {
+    frameSize = 0;
+    rtpFile.open(QIODevice::WriteOnly | QIODevice::Text);
+}
+//-------------------------------------------------------------------
+protocol_rtp::~protocol_rtp()
+{
+    rtpFile.close();
 }
 //-------------------------------------------------------------------
 int
@@ -11,13 +18,28 @@ protocol_rtp::check(u_int16_t protocol, u_int16_t sport, u_int16_t dport, const 
        (sport == source_port && dport == destination_port) ||
        (dport == source_port && sport == destination_port))
     {
+        RFC2250H *rfc2250_h = (RFC2250H *)(payload + 12);
         statistic->incrementCounter(CNT_RTP);
+        frameSize += payload_length - 8 - 12 - 4; // UDP, RTP and rfc2250 Header Length
+        if (rfc2250_h->picture_coding_type & 0x08)
+            process((rfc2250_h->picture_coding_type & 0x07), (ntohs(rfc2250_h->temporal_reference) & 0x03ff));
     }
 }
 //-------------------------------------------------------------------
 int
-protocol_rtp::process()
+protocol_rtp::process(__uint16_t fType, __uint16_t tempRef)
 {
+//    std::cout<<"--->"<< (fType==1? "I":(fType==2?"P":"B"))
+//             <<" / " << tempRef
+//             <<" / " << frameSize
+//             <<std::endl;
 
+    QTextStream rtpOut(&rtpFile);
+    QString tmp =  (fType==1? "I ":(fType==2?"P ":"B "));
+            tmp += QString::number(tempRef) + " ";
+            tmp += QString::number(frameSize) + " ";
+    rtpOut << tmp + "\n";
+    rtpOut.flush();
+    frameSize = 0;
 }
 //-------------------------------------------------------------------
--- protocol_rtp.h
+++ protocol_rtp.h
@@ -1,14 +1,38 @@
 #ifndef PROTOCOL_RTP_H
 #define PROTOCOL_RTP_H
 #include "protocolhandler.h"
+#include <QFile>
 
+struct RFC2250H
+{
+ u_int16_t temporal_reference;
+ u_char    picture_coding_type;
+ u_char    vector_code_bits; // FBV,BFC,FFV,FFC from RFC 2250, sec. 3.4
+// u_int8_t MBZ:5;
+// u_int8_t T:1;
+// u_int16_t TemporalRefrence:10;
+// u_int8_t AN:1;
+// u_int8_t NewPictureHeader:1;
+// u_int8_t SequenceHeader:1;
+// bool     BeginningofSlice:1;
+// bool     EndofSlice:1;
+// u_int8_t PictureType:3;
+// u_int8_t FBV:1;
+// u_int8_t BFC:3;
+// u_int8_t FFV:1;
+// u_int8_t FFC:3;
+};
 
 class protocol_rtp : public protocolHandler_
 {
 public:
             protocol_rtp(u_int16_t sport, u_int16_t dport,statistic_ *stat);
+            ~protocol_rtp();
     int     check(u_int16_t protocol, u_int16_t sport, u_int16_t dport, const u_char *payload, unsigned int payload_length);
-    int     process();
+    int     process(__uint16_t fType, __uint16_t tempRef);
+private:
+    QFile       rtpFile;
+    __uint32_t  frameSize;
 };
 
 #endif // PROTOCOL_RTP_H
